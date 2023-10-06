ee467e7b3cc6cc29bf0b0c7ee9d68f7d1462ba70
root <root@PHAM-DUC-HAO.localdomain>
2023-10-06 11:22:51 +0700

    Passed n=100.000: 750s

--- n_queens.py
+++ n_queens.py
@@ -23,8 +23,10 @@ class NQueens:
                 pass
             case 'queen_search2':
                 self._queen_search2()
+            case 'trivial':
+                self._trivial()
             case _:
-                print('[!] Unknown algorithm')
+                # print('[!] Unknown algorithm')
                 return False
         assert self.is_safe(self.queen)
         return True
@@ -68,45 +70,16 @@ class NQueens:
         """
         Based on the paper: Fast Search Algorithms for the N-Queens Problem
         """
-        def swap_ok(i, j, queen, collisions, dn, dp):  
-            new_collisions = collisions
-            if dn[i + queen[i]] == 1:
-                new_collisions += 1
-            if dn[j + queen[j]] == 1:
-                new_collisions += 1
-            if dp[i - queen[i] - (1 - self.size)] == 1:
-                new_collisions += 1
-            if dp[j - queen[j] - (1 - self.size)] == 1:
-                new_collisions += 1
-            if dn[i + queen[j]] == 0:
-                new_collisions -= 1
-            if dn[j + queen[i]] == 0:
-                new_collisions -= 1
-            if dp[i - queen[j] - (1 - self.size)] == 0:
-                new_collisions -= 1
-            if dp[j - queen[i] - (1 - self.size)] == 0:
-                new_collisions -= 1
-            if new_collisions < collisions:
-                return True
-            return False
-        
-        def perform_swap(i, j, queen, collisions, dn, dp):
-            if dn[i + queen[i]] == 1:
-                collisions += 1
-            if dn[j + queen[j]] == 1:
-                collisions += 1
-            if dp[i - queen[i] - (1 - self.size)] == 1:
-                collisions += 1
-            if dp[j - queen[j] - (1 - self.size)] == 1:
-                collisions += 1
-            if dn[i + queen[j]] == 0:
-                collisions -= 1
-            if dn[j + queen[i]] == 0:
-                collisions -= 1
-            if dp[i - queen[j] - (1 - self.size)] == 0:
-                collisions -= 1
-            if dp[j - queen[i] - (1 - self.size)] == 0:
-                collisions -= 1
+        def swap_ok(i, j, queen, dn, dp):  
+            idx_dn = set([i + queen[i], j + queen[j], i + queen[j], j + queen[i]])
+            idx_dp = set([i - queen[i], j - queen[j], i - queen[j], j - queen[i]])
+            diff = 0
+            for idx in idx_dn:
+                if dn[idx] > 0:
+                    diff += dn[idx] - 1
+            for idx in idx_dp:
+                if dp[idx - (1 - self.size)] > 0:
+                    diff += dp[idx - (1 - self.size)] - 1
             dn[i + queen[i]] -= 1
             dn[j + queen[j]] -= 1
             dn[i + queen[j]] += 1
@@ -117,7 +90,55 @@ class NQueens:
             dp[i - queen[j] - (1 - self.size)] += 1
             dp[j - queen[i] - (1 - self.size)] += 1
 
+            for idx in idx_dn:
+                if dn[idx] > 0:
+                    diff -= dn[idx] - 1
+            for idx in idx_dp:
+                if dp[idx - (1 - self.size)] > 0:
+                    diff -= dp[idx - (1 - self.size)] - 1
+
+            dn[i + queen[i]] += 1
+            dn[j + queen[j]] += 1
+            dn[i + queen[j]] -= 1
+            dn[j + queen[i]] -= 1
+
+            dp[i - queen[i] - (1 - self.size)] += 1
+            dp[j - queen[j] - (1 - self.size)] += 1
+            dp[i - queen[j] - (1 - self.size)] -= 1
+            dp[j - queen[i] - (1 - self.size)] -= 1
+            if diff > 0:
+                return True
+            return False
+        
+        def perform_swap(i, j, queen, collisions, dn, dp):
+            idx_dn = set([i + queen[i], j + queen[j], i + queen[j], j + queen[i]])
+            idx_dp = set([i - queen[i], j - queen[j], i - queen[j], j - queen[i]])
+            diff = 0
+            for idx in idx_dn:
+                if dn[idx] > 0:
+                    diff += dn[idx] - 1
+            for idx in idx_dp:
+                if dp[idx - (1 - self.size)] > 0:
+                    diff += dp[idx - (1 - self.size)] - 1
+            # perform swap
+            dn[i + queen[i]] -= 1
+            dn[j + queen[j]] -= 1
+            dn[i + queen[j]] += 1
+            dn[j + queen[i]] += 1
+            dp[i - queen[i] - (1 - self.size)] -= 1
+            dp[j - queen[j] - (1 - self.size)] -= 1
+            dp[i - queen[j] - (1 - self.size)] += 1
+            dp[j - queen[i] - (1 - self.size)] += 1
+
+            for idx in idx_dn:
+                if dn[idx] > 0:
+                    diff -= dn[idx] - 1
+            for idx in idx_dp:
+                if dp[idx - (1 - self.size)] > 0:
+                    diff -= dp[idx - (1 - self.size)] - 1
+
             queen[i], queen[j] = queen[j], queen[i]
+            collisions -= diff
             return collisions
         
         def compute_collisions(queen):
@@ -127,13 +148,7 @@ class NQueens:
                 col = queen[row]
                 dn[row + col] += 1
                 dp[row - col - (1 - self.size)] += 1
-            collisions = 0
-            for x in dn:
-                if x > 0:
-                    collisions += x - 1
-            for x in dp:
-                if x > 0:
-                    collisions += x - 1
+            collisions = sum([x-1 for x in dn if x > 0]) + sum([x-1 for x in dp if x > 0])
             return collisions, dn, dp
         
         def compute_attacks(queen, dn, dp):
@@ -146,6 +161,7 @@ class NQueens:
         
         collisions = -1
         while collisions != 0:
+            # print(collisions)
             # Generate a random permutation of queen[0] to queen[size-1]
             queen = list(range(self.size))
             random.shuffle(queen)
@@ -154,10 +170,13 @@ class NQueens:
             number_of_attacks, attack = compute_attacks(queen, dn, dp)
             loopcount = 0
             while loopcount <= c2 * self.size:
+                if number_of_attacks == 0:
+                    self.queen = queen
+                    return
                 for k in range(number_of_attacks):
                     i = attack[k]
                     j = random.choice(range(self.size))
-                    if swap_ok(i, j, queen, collisions, dn, dp):
+                    if swap_ok(i, j, queen, dn, dp):
                         collisions = perform_swap(i, j, queen, collisions, dn, dp)
                         if collisions == 0:
                             self.queen = queen
